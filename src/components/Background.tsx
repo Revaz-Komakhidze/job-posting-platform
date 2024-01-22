8c956142057669a3eb325ecdb452ff3b1200a5fa
Rezo Komakhidze <rezokomakhidze1@gmail.com>
2024-01-22 22:25:57 +0400

    added components and css for backgorund

---
+++ src/components/Background.tsx
@@ -0,0 +1,10 @@
+export default function Background() {
+  return (
+    <div className="background">
+      <img
+        src="../../public/pattern.svg"
+        alt="Background pattern"
+      />
+    </div>
+  );
+}
---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>
+        <p>
+          © Copyright by{" "}
+          <a href="." target="_blank">
+            Company Website
+          </a>
+          . Intended for improving React skills.
+        </p>
+      </small>
+
+      <p>
+        <span className="u-bold">109573</span> total jobs available
+      </p>
+    </footer>
+  );
+}
