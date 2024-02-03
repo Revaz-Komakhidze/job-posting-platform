fef86cddbd3218c48dafc867a5266c3f755c7757
Rezo Komakhidze <rezokomakhidze1@gmail.com>
2024-02-03 11:17:00 +0400

    added custom hook for fetching job item

---
+++ src/libs/hooks.ts
@@ -0,0 +1,40 @@
+import { JobItemExpanded } from "./types";
+import { BASE_API_URL } from "./constants";
+import { useQuery } from "@tanstack/react-query";
+import { handleError } from "./utils";
+
+type JobItemApiResponse = {
+  public: boolean;
+  jobItem: JobItemExpanded;
+};
+
+const fetchJobItem = async (id: number): Promise<JobItemApiResponse> => {
+  const response = await fetch(`${BASE_API_URL}/${id}`);
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.description);
+  }
+
+  const data = await response.json();
+  return data;
+};
+
+export function useJobItem(id: number | null) {
+  const { data, isInitialLoading } = useQuery(
+    ["job-item", id],
+    () => (id ? fetchJobItem(id) : null),
+    {
+      staleTime: 1000 * 60 * 60,
+      refetchOnWindowFocus: false,
+      retry: false,
+      enabled: Boolean(id),
+      onError: handleError,
+    }
+  );
+
+  return {
+    jobItem: data?.jobItem,
+    isLoading: isInitialLoading,
+  } as const;
+}
