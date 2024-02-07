736a045b9562a41d06fb04269b7b206eb965c68b
Rezo Komakhidze <rezokomakhidze1@gmail.com>
2024-02-07 11:32:00 +0400

    added hook for keeping track of an active if

--- src/libs/hooks.ts
+++ src/libs/hooks.ts
@@ -115,3 +115,23 @@ export function useDebounce<T>(value: T, delay = 500): T {
 
   return debouncedValue;
 }
+
+export function useActiveId() {
+  const [activeId, setActiveId] = useState<number | null>(null);
+
+  useEffect(() => {
+    const handleHashChange = () => {
+      const id = +window.location.hash.slice(1);
+      setActiveId(id);
+    };
+    handleHashChange();
+
+    window.addEventListener("hashchange", handleHashChange);
+
+    return () => {
+      window.removeEventListener("hashchange", handleHashChange);
+    };
+  }, []);
+
+  return activeId;
+}
