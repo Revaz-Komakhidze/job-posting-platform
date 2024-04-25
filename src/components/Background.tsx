d5f0b324961eccaa34727993a8fd4ee9d9467e83
Rezo Komakhidze <rezokomakhidze1@gmail.com>
2024-04-25 11:08:54 +0400

    created background component

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,10 +1,7 @@
 export default function Background() {
   return (
     <div className="background">
-      <img
-        src="/pattern.svg"
-        alt="Background pattern"
-      />
+      <img src="/pattern.svg" alt="Background pattern" />
     </div>
   );
 }
